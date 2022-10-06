6c605bdbecd673acdc1ab5ebe768a33e64571ccc
jungHyeonS <fbtls25@naver.com>
2022-10-06 22:23:44 +0900

    c++ : 출력

---
+++ basic/output.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// cout 실수형 출력
+// typedef long long ll;
+// double a = 1.2345678;
+// int main(){
+//     cout << a << "\n";
+//     cout.precision(7);
+//     cout << a << "\n";
+//     return 0;
+// }
+
+
+//printf
+typedef long long ll;
+double a= 1.2345678;
+int b = 2;
+int main(){
+    printf("%.6lf\n",a);
+    printf("%02d\n",b);
+    return 0;
+}
