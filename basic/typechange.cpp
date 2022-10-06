c222975e493559f5b999c9e04acfda6f6d63aa8d
jungHyeonS <fbtls25@naver.com>
2022-10-06 22:29:34 +0900

    c++ : 형변환

---
+++ basic/typechange.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//double to int
+// int main(){
+//     double ret = 2.12345;
+//     int n = 2;
+//     int a = (int)round(ret / double(n));
+//     cout << a << "\n";
+//     return 0;
+// }
+
+
+//문자를 숫자로, 숫자를 문자로
+int main(){
+    char a = 'a';
+    cout << (int)a - 97 << "\n";
+    return 0;
+}
