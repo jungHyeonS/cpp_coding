bec04d0e60bfeae652f763f5ba49f7081f590fdf
jungHyeonS <fbtls25@naver.com>
2022-10-26 22:49:26 +0900

    cpp : 재귀함수 순열 구현

--- basic/f.cpp
+++ basic/f.cpp
@@ -26,12 +26,30 @@ void printV(vector<int> &v){
     cout << "\n";
 }
 
+
+void makePermutation(int n,int r, int depth){
+    cout << n << " : " << r << " : " << depth << "\n";
+    if(r == depth){
+        printV(v);
+        return ;
+    }
+    //depth를 기반으로 swap
+    for(int i = depth; i < n; i++){
+        swap(v[i],v[depth]);
+        makePermutation(n,r,depth + 1);
+        swap(v[i],v[depth]);
+    }
+    return;
+}
+
+
 int main(){
     for (int i = 1; i <=3; i++)v.push_back(i);
 
-    do{
-        printV(v);
-    }while(next_permutation(v.begin(),v.end()));
+    makePermutation(3,3,0);
+    // do{
+    //     printV(v);
+    // }while(next_permutation(v.begin(),v.end()));
 
     return 0;
 }
