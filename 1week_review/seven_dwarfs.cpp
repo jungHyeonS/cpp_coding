e14487d7331aa3e839181ecb4d5a87d2be79e14a
jungHyeonS <fbtls25@naver.com>
2022-11-12 16:01:44 +0900

    cpp : 일곱난쟁이 주석 추가

--- 1week_review/seven_dwarfs.cpp
+++ 1week_review/seven_dwarfs.cpp
@@ -3,17 +3,27 @@ using namespace std;
 
 int arr[9],sum;
 int main(){
+
+    //9개의 정수를 입력 받는다
     for(int i = 0; i < 9; i++){
         cin >> arr[i];
     }
 
+
+    //순열을 정렬이 되어있어야 되기때문에 정렬을 한다,또한 문제에 오름차순이라고 되어있다
     sort(arr,(arr+9));
 
+
+    //next_permutation 함수로 9개의 숫자에 대한 순열을 뽑는다
     do{
+
+        //뽑아진 순열에서 7개를 가져와 sum에 더해준다
         sum = 0;
         for(int j = 0; j < 7; j++){
             sum += arr[j];
         }  
+
+        //만약 sum에 합이 100일때 해당 순열을 출력하고 종료한다
         if(sum == 100){
             for(int j = 0; j < 7; j++){
                 cout << arr[j] << "\n";
@@ -21,6 +31,7 @@ int main(){
             break;
         }
     }while(next_permutation(arr,(arr+9)));
+    
 
     
     return 0;
